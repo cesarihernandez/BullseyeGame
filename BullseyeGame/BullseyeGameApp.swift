a1f30648023cbe6ffdc4d3db52f0f19ef613642d
Cesar Hernandez <cesarihernandez92@gmail.com>
2024-02-12 15:05:30 -0800

    Initial Commit

---
+++ BullseyeGame/BullseyeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeGameApp.swift
+//  BullseyeGame
+//
+//  Created by Cesar Hernandez on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
